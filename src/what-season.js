0685fc2727fbe0df9fc88d78c3f610df374f20f8
Katerina <comina.ru@gmail.com>
2022-05-02 23:24:39 +0300

    refactor: update what-season

--- src/what-season.js
+++ src/what-season.js
@@ -15,40 +15,25 @@ function getSeason(date) {
   // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
   let season;
-  // if (date) {
-  //   let month = date.getMonth();
-  //   if (month === 11 || month === 0 || month === 1) {
-  //     season = 'winter';
-  //   } else if (month === 2 || month === 3 || month === 4) {
-  //     season = 'spring';
-  //   } else if (month === 5 || month === 6 || month === 7) {
-  //     season = 'summer';
-  //   } else if (month === 8 || month === 9 || month === 10) {
-  //     season = 'autumn';
-  //   } else {
-  //     throw new Error('Invalid date!')
-  //   }
-  // } else {
-  //   season = 'Unable to determine the time of year!';
-  // }
+  if(!date) {
+    return 'Unable to determine the time of year!'
+  }
 
   try {
-    if (date) {
-      let month = date.getMonth();
-      if (month === 11 || month === 0 || month === 1) {
-        season = 'winter';
-      } else if (month === 2 || month === 3 || month === 4) {
-        season = 'spring';
-      } else if (month === 5 || month === 6 || month === 7) {
-        season = 'summer';
-      } else {
-        season = 'autumn';
-      }
-    } else {
-      season = 'Unable to determine the time of year!';
-    }
-  } catch {
+    date.getTime();
+  } catch (error) {
     throw new Error('Invalid date!');
+  }
+
+  let month = date.getMonth();
+  if (month === 11 || month === 0 || month === 1) {
+    season = 'winter';
+  } else if (month === 2 || month === 3 || month === 4) {
+    season = 'spring';
+  } else if (month === 5 || month === 6 || month === 7) {
+    season = 'summer';
+  } else {
+    season = 'autumn';
   }
   
   return season
