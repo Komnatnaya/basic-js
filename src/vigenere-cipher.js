13f34cd3841035338979037457611a891b33923d
Katerina <comina.ru@gmail.com>
2022-10-17 20:45:30 +0300

    feat: add vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -24,18 +24,76 @@ class VigenereCipheringMachine {
     this.x = x;
   }
   encrypt(message, key) {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let result = '';
+
     if (!message || !key) {
       throw new Error('Incorrect arguments!');
     }
 
+    let arrMessage = message.toUpperCase().split('');
+    let arrKey = key.toUpperCase().split('');
+    let arrNumber = [];
+    for(let i = 0, j = -1; i < arrMessage.length; i++) {
+      if(letters.includes(arrMessage[i])) {
+        j++;
+        if(j >= key.length) { 
+          j = j % key.length; 
+        }
+        arrNumber.push(letters[(letters.indexOf(arrMessage[i]) + letters.indexOf(arrKey[j])) % 26]);
+      } else {
+        arrNumber.push(arrMessage[i]);
+      }
+    }
+
+    if(this.x) {
+      result = arrNumber.join('');
+    } else {
+      result = arrNumber.reverse().join('');;
+    }
+
     return result
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
+  decrypt(encryptedMessage, key) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let result = '';
+
+    if (!encryptedMessage || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let arrEncryptedMessage = encryptedMessage.toUpperCase().split('');
+    let arrKey = key.toUpperCase().split('');
+    let arrNumber = [];
+    for(let i = 0, j = -1; i < arrEncryptedMessage.length; i++) {
+      if(letters.includes(arrEncryptedMessage[i])) {
+        j++;
+        if(j >= key.length) { 
+          j = j % key.length; 
+        }
+        let numberLetter = letters.indexOf(arrEncryptedMessage[i]) - letters.indexOf(arrKey[j]);
+        if (numberLetter < 0) {
+          arrNumber.push(letters[numberLetter + 26]);
+        } else {
+          arrNumber.push(letters[numberLetter]);
+        }
+        
+      } else {
+        arrNumber.push(arrEncryptedMessage[i]);
+      }
+    }
+
+    if(this.x) {
+      result = arrNumber.join('');
+    } else {
+      result = arrNumber.reverse().join('');;
+    }
+
+    return result
   }
 }
 
