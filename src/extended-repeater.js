7bf752e495f600467850ac8c898428a8e0a7986f
Katerina <comina.ru@gmail.com>
2025-11-09 22:13:15 +0200

    feat: add extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -16,9 +16,35 @@ const { NotImplementedError } = require('../lib');
  *
  */
 
-function repeater(/* str, options */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function repeater(str, options) {
+  let repeatStr;
+  let arr = [];
+  let additionArr = [];
+  let text = String(str);
+
+  let separatorStr = options.separator ? options.separator : '+';
+  let additionStr = (options.addition === undefined) ? '' : String(options.addition);
+  let additionSeparatorStr = options.additionSeparator ? options.additionSeparator : '|';
+  let additionRepeatTimesStr = options.additionRepeatTimes ? options.additionRepeatTimes : 1;
+
+  for (let i = 0; i < additionRepeatTimesStr; i++){
+    additionArr.push(additionStr);
+  }
+  let additionAllStr = additionArr.join(additionSeparatorStr);
+
+  let repeatTimesStr = options.repeatTimes ? options.repeatTimes : 1;
+
+  for (let i = 0; i < repeatTimesStr; i++){
+    if (additionAllStr.length > 0) {
+      arr.push(text + additionAllStr);
+    } else {
+      arr.push(text);
+    }
+  }
+
+  repeatStr = arr.join(separatorStr);
+
+  return repeatStr;
 }
 
 module.exports = {
