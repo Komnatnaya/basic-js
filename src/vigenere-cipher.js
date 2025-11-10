6e391fea91a8901c4d27a8eaaf37dfa5ac8780a5
Katerina <comina.ru@gmail.com>
2025-11-10 17:10:01 +0200

    feat: add vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,76 @@ const { NotImplementedError } = require('../lib');
  *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  constructor(x = true) {
+    this.x = x;
   }
+  encrypt(message, key) {
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let result = '';
 
-  decrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let arrMessage = message.toUpperCase().split('');
+    let arrKey = key.toUpperCase().split('');
+    let arrNumber = [];
+    for(let i = 0, j = -1; i < arrMessage.length; i++) {
+      if(letters.includes(arrMessage[i])) {
+        j++;
+        if(j >= key.length) { 
+          j = j % key.length; 
+        }
+        arrNumber.push(letters[(letters.indexOf(arrMessage[i]) + letters.indexOf(arrKey[j])) % 26]);
+      } else {
+        arrNumber.push(arrMessage[i]);
+      }
+    }
+
+    if(this.x) {
+      result = arrNumber.join('');
+    } else {
+      result = arrNumber.reverse().join('');;
+    }
+
+    return result
+  }
+  decrypt(encryptedMessage, key) {
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let result = '';
+
+    if (!encryptedMessage || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let arrEncryptedMessage = encryptedMessage.toUpperCase().split('');
+    let arrKey = key.toUpperCase().split('');
+    let arrNumber = [];
+    for(let i = 0, j = -1; i < arrEncryptedMessage.length; i++) {
+      if(letters.includes(arrEncryptedMessage[i])) {
+        j++;
+        if(j >= key.length) { 
+          j = j % key.length; 
+        }
+        let numberLetter = letters.indexOf(arrEncryptedMessage[i]) - letters.indexOf(arrKey[j]);
+        if (numberLetter < 0) {
+          arrNumber.push(letters[numberLetter + 26]);
+        } else {
+          arrNumber.push(letters[numberLetter]);
+        }
+        
+      } else {
+        arrNumber.push(arrEncryptedMessage[i]);
+      }
+    }
+
+    if(this.x) {
+      result = arrNumber.join('');
+    } else {
+      result = arrNumber.reverse().join('');;
+    }
+
+    return result
   }
 }
 
